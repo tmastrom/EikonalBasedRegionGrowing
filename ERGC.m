5e040fd17e751c1dbe4fdb400e45385f7aaefdbd
tmastrom <thomas.mastromonaco@gmail.com>
2019-04-01 18:18:54 -0700

    removed toy test images, cleaned up comments

--- ERGC.m
+++ ERGC.m
@@ -9,22 +9,24 @@ close all
 W = size(img, 1);       % image height
 H = size(img, 2);       % image width
 
+% Display Input Image 
 figure('Name','Input Image')
 imshow(img, [], 'InitialMagnification' ,'fit')
 
+% Let user pick seed pixel 
 [yi, xi] = getpts;
 xi = int8(xi);
 yi = int8(yi);
 
 
-% initialize seeds in a grid 
+% Initialize seeds in a grid 
 % [Seeds, Seed_map] = placeSeedsOnGrid(img, K); 
 Seeds = struct('x',xi,'y',yi,'dist',0, 'label', 1);      % x coord, ycoord, label: 0 = seed
 Seed_map = -1*ones([size(img, 1), size(img, 2)]); 
 Seed_map(xi,yi) = 0;
 
 
-% initialize Distance and State images
+% Initialize Distance and State images
 Dist_map = 100000*ones([W, H]);   
 % State map
 % -1 = COMPUTED
@@ -36,11 +38,11 @@ State_map(xi,yi) = 0; % 0 = ALIVE, 1 = FAR AWAY, -1 = COMPUTED
 %[Dist_map, State_map] = initializeImages(Seeds, Dist_map, State_map);
 
 
-% initialize superpixels
+% Initialize Superpixels
 Superpixels = 255*ones([size(img, 1), size(img, 2)]);
 [SPs, Superpixels] = initializeSuperpixels(img, Seeds, State_map, Superpixels);
 
-% fast marching algorithm
+% Perform Fast Marching Algorithm
 fastMarchingAlg(Dist_map, Seeds, State_map, img, SPs, Superpixels, Seed_map);
 
 
--- computeDistance.m
+++
@@ -1,8 +0,0 @@
-function d = computeDistance(c1, c2)
-%fprintf('c1, %d ', c1)    
-%fprintf('c2, %d \n', c2)  
-d = (c1 - c2)*(c1 - c2);
-% 
-% d = norm( (c1-c2), 2);
-% d = d^2;
-
